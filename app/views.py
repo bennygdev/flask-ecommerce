3d38bb7c58f31131ed7853f5d35977d162d762ba
Benny Goh <haiguan648@gmail.com>
2024-09-27 08:01:03 +0800

    added routes, added active links

--- app/views.py
+++ app/views.py
@@ -5,3 +5,15 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
   return render_template("home.html")
+
+@views.route('/products')
+def products():
+  return render_template("products.html")
+
+@views.route('/saved')
+def saved():
+  return render_template("saved.html")
+
+@views.route('/cart')
+def cart():
+  return render_template("cart.html")
