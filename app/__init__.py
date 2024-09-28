b9e5d04349647158b4b6249d8908b96d40a7dcc2
Benny Goh <haiguan648@gmail.com>
2024-09-29 01:04:30 +0800

    insert admin and owner accounts

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from werkzeug.security import generate_password_hash
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -41,6 +42,8 @@ def create_database(app):
       db.create_all()
       print('Created Database!')
       insert_default_roles()
+      insert_users()
+      
 
 def insert_default_roles():
   from .models import Role
@@ -54,3 +57,47 @@ def insert_default_roles():
     
   db.session.commit()
   print('Inserted default roles into the database!')
+
+# insert admin and owner accounts here
+def insert_users():
+  from .models import User
+  admin1 = User(
+    first_name = "Admin", 
+    last_name = "1",
+    username = "admin1",
+    email = "admin1@gmail.com",
+    password = generate_password_hash("admin1", method='pbkdf2:sha256'),
+    role_id = 2
+  )
+
+  admin2 = User(
+    first_name = "Admin", 
+    last_name = "2",
+    username = "admin2",
+    email = "admin2@gmail.com",
+    password = generate_password_hash("admin2", method='pbkdf2:sha256'),
+    role_id = 2
+  )
+
+  owner = User(
+    first_name = "Owner", 
+    last_name = "3",
+    username = "owner",
+    email = "owner@gmail.com",
+    password = generate_password_hash("ownerApp", method='pbkdf2:sha256'),
+    role_id = 3
+  )
+
+  users = [
+    ("admin1@gmail.com", admin1),
+    ("admin2@gmail.com", admin2),
+    ("owner@gmail.com", owner)
+  ]
+
+  for email, user in users:
+    existing_user = User.query.filter_by(email=email).first()
+    if not existing_user:
+      db.session.add(user)
+
+  db.session.commit()
+  print('Inserted Admin and Owner accounts!')
