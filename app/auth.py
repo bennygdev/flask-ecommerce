82ebc9a091b225435b954d899ad3c434c93e0e9b
Benny Goh <haiguan648@gmail.com>
2024-09-28 14:58:28 +0800

    added server side validation without the sessions yet

--- app/auth.py
+++ app/auth.py
@@ -15,7 +15,26 @@ def login():
 def logout():
   return "Log out"
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+  if request.method == 'POST':
+    firstName = request.form.get('firstName')
+    lastName = request.form.get('lastName')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    confirmPassword = request.form.get('confirmPassword')
+    username = request.form.get('username')
+
+    # print(request.form)
+    # server side validation
+    if (not firstName or len(firstName) < 1 or not lastName or len(lastName) < 1 or
+        not email or len(email) < 1 or not password or len(password) < 6 or 
+        password != confirmPassword or not username or len(username) < 1):
+      print('invalid')
+      return
+    else:
+      # auth side not added yet
+      return redirect(url_for('views.home')) 
+    
   return render_template("register.html")
 
